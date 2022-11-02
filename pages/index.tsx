4ac7bf680770077a5819464cc4e768f52b0c3345
Bruno Brian Sousa <bruninho_brian@hotmail.com>
2022-11-01 23:15:53 -0300

    adjust

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,9 +14,6 @@ export const getServerSideProps = async () => {
   const response = await fetch('http://localhost:3333/pools/count');
   const data = await response.json();
 
-  console.log(data);
-  
-
   return {
     props: {
       count: data
