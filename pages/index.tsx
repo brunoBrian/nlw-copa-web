1061b7c6286e84d48cb380dc697488d4c982d55d
Bruno Brian Sousa <bruninho_brian@hotmail.com>
2022-11-01 23:05:28 -0300

    consuming pool/count service

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,25 @@
-export default function Home() {
+type HomeProps = {
+  count: number
+}
+
+export default function Home({count}: HomeProps) {
+  fetch('http://localhost:3333/pools/count')
+
   return (
-    <h1>Hello</h1>
+    <h1>Hello {count}</h1>
   )
+}
+
+export const getServerSideProps = async () => {
+  const response = await fetch('http://localhost:3333/pools/count');
+  const data = await response.json();
+
+  console.log(data);
+  
+
+  return {
+    props: {
+      count: data
+    }
+  }
 }
